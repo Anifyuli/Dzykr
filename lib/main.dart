fdd150c5342ed8e4dab227f5de7787146a498ba1
Anifyuli <anifyuliansyah@gmail.com>
2023-07-27 09:38:33 +0700

    Starting slicing UI

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,53 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dzykr',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
-        useMaterial3: true,
+        fontFamily: 'Roboto',
       ),
-      home: const Placeholder(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Dzykr'),
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                '99',
+                style: TextStyle(fontSize: 120),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: null,
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(32, 20, 32, 20)), // Mengatur jarak antara ikon dan tepi tombol
+                  backgroundColor: MaterialStateProperty.all(Colors.green), // Warna latar belakang tombol ketika diangkat
+                ),
+                child: const Icon(
+                  Icons.add,
+                  color: Colors.white,
+                  size: 32,
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: null,
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(const CircleBorder()),
+                  padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(32, 20, 32, 20)),
+                  backgroundColor: MaterialStateProperty.all(Colors.red), 
+                ),
+                child: const Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                  size: 32,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
