9c257a2e04ab54332f3f6f9229d45fe28628b01f
Anifyuli <anifyuliansyah@gmail.com>
2023-07-28 16:09:24 +0700

    Fix Reset Button Position

--- lib/main.dart
+++ lib/main.dart
@@ -22,16 +22,17 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              const Spacer(),
               const Text(
                 '99',
                 style: TextStyle(fontSize: 120),
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 20,),
               ElevatedButton(
                 onPressed: null,
                 style: ButtonStyle(
-                  padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(32, 20, 32, 20)), // Mengatur jarak antara ikon dan tepi tombol
-                  backgroundColor: MaterialStateProperty.all(Colors.green), // Warna latar belakang tombol ketika diangkat
+                  padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(32, 20, 32, 20)),
+                  backgroundColor: MaterialStateProperty.all(Colors.green),
                 ),
                 child: const Icon(
                   Icons.add,
@@ -39,18 +40,22 @@ class MyApp extends StatelessWidget {
                   size: 32,
                 ),
               ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: null,
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all(const CircleBorder()),
-                  padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(32, 20, 32, 20)),
-                  backgroundColor: MaterialStateProperty.all(Colors.red), 
-                ),
-                child: const Icon(
-                  Icons.delete,
-                  color: Colors.white,
-                  size: 32,
+              const Spacer(),
+              Container(
+                alignment: Alignment.bottomRight,
+                margin: const EdgeInsets.only(bottom: 30, right: 20),
+                child: ElevatedButton(
+                  onPressed: null,
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(const CircleBorder()),
+                    padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(32, 20, 32, 20)),
+                    backgroundColor: MaterialStateProperty.all(Colors.red),
+                  ),
+                  child: const Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                    size: 32,
+                  ),
                 ),
               ),
             ],
